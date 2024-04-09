048da1136c2f8ad496c2e55d041d381f653b4b54
tgunnoe <taylor.gunnoe@iohk.io>
2024-04-09 14:24:46 -0400

    Remove comment from nameservers

--- systems/chapterhouse.nix
+++ systems/chapterhouse.nix
@@ -133,7 +133,7 @@
     # '';
   };
   networking.nameservers =
-    [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    [ "1.1.1.1" "1.0.0.1" ];
 
   networking = {
     hostId = "e53dd769";
